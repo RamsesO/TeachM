382253d53ba0ad4dbcc64a54a07badfa418fa174
Sina Eradat <Sina@sinas-macbook-pro.local>
2017-09-25 10:18:13 -0700

    add the main view controllers and there classes

--- TeachM/AssignmentsViewController.swift
+++ TeachM/AssignmentsViewController.swift
@@ -1,44 +1,14 @@
 //
-//  LogInViewController.swift
+//  AssignmentsViewController.swift
 //  TeachM
 //
-//  Created by Sina Eradat on 9/20/17.
+//  Created by Sina Eradat on 9/25/17.
 //  Copyright © 2017 Sina Eradat. All rights reserved.
 //
 
 import UIKit
 
-class LogInViewController: UIViewController {
-    
-    let master_UserName = "Sina"
-    let master_Password = "1234"
-    
-    @IBOutlet weak var user_Name: UITextField!
-    
-    @IBOutlet weak var password: UITextField!
-    
-    @IBAction func logIn(_ sender: UIButton) {
-        if user_Name.text! == master_UserName{
-            if password.text! == master_Password{
-                
-            }//end password check
-        }//end user name check
-    }//end log in
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+class AssignmentsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
