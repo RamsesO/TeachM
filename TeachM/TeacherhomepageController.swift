6b8bef72e28148dc0ad2d5d6b6e5ceae48aaced8
yanyan4976 <yanyan@yans-macbook-pro.local>
2017-09-28 09:48:05 -0700

    teacherpageController

---
+++ TeachM/TeacherhomepageController.swift
@@ -0,0 +1,32 @@
+//
+//  TeacherhomepageContriller.swift
+//  TeachM
+//
+//  Created by Yan Yan on 9/28/17.
+//  Copyright © 2017 Yan Yan. All rights reserved.
+//
+
+import UIKit
+class TeacherhomepageController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        public class teacherInfo {
+            
+            
+            
+            }
+        
+        // load the view first
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+}
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+}
+
+
