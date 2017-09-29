a83fe7c5b2b05316dd57d4e156d47be16fd8d444
yanyan4976 <yanyan@Yans-MacBook-Pro.local>
2017-09-29 14:14:34 -0700

    just did the outlet on the teacher's homepage

--- TeachM/TeacherhomepageController.swift
+++ TeachM/TeacherhomepageController.swift
@@ -18,10 +18,3 @@ class TeacherhomepageController : UIViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
 }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
-        // Dispose of any resources that can be recreated.
-}
-
-
