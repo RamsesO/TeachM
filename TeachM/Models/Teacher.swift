abac14dccc57e56f394707ec6dc7e4557f74d293
Ramses <ordonez.ramses@gmail.com>
2017-09-29 17:57:38 -0700

    modified structs

--- TeachM/Models/Teacher.swift
+++ TeachM/Models/Teacher.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import Firebase
+
+struct Teacher{
+    
+    let key: String
+    let ref: DatabaseReference?
+    let name: String
+    // also has list of classes
+    
+    init(name: String, key: String = "") {
+        self.key = key
+        self.name = name
+        self.ref = nil
+    }
+    
+    init(snapshot: DataSnapshot){
+        key = snapshot.key
+        let snapshotValue = snapshot.value as! [String: AnyObject]
+        name = snapshotValue["name"] as! String
+        
+        // list of classes of the teacher
+        ref = snapshot.ref
+        
+    }
+}
