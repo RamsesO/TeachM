3c825e217b968c2e3971430ce1a8445df44c8f96
yanyan4976 <yan.yan.71@my.csun.edu>
2017-10-02 10:29:09 -0700

    edit teacher homepage

--- TeachM/teacherhomepge/TeacherhomepageController.swift
+++ TeachM/teacherhomepge/TeacherhomepageController.swift
@@ -9,36 +9,33 @@
 import UIKit
 class TeacherhomepageController : UIViewController {
     
-    @IBOutlet weak var img: UIImageView!
-    
+    @IBOutlet weak var jeffimg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        jeffimg.layer.cornerRadius = jeffimg.frame.size.width/2
+        jeffimg.clipsToBounds = true
         
-        img.layer.cornerRadius = img.frame.size.width/2
-        img.clipsToBounds = true
         
-        
-        
+    
         // load the view first
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+   
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}
+  
+  
 
+
+
+
+
+}
